1ccf4ebcf04a4526c4ac89194870de175ae99f77
Thomas Bille <thomas.bille@canonical.com>
2019-02-14 16:42:30 +0000

    Add base templates for blog

--- webapp/app.py
+++ webapp/app.py
@@ -10,11 +10,20 @@ from webapp.blueprint import jp_website
 from webapp.handlers import set_handlers
 
 
+from werkzeug.routing import BaseConverter
+
+
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+
 app = flask.Flask(
     __name__, template_folder="../templates", static_folder="../static"
 )
 
 app.url_map.strict_slashes = False
+app.url_map.converters["regex"] = RegexConverter
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 if app.debug:
@@ -22,6 +31,10 @@ if app.debug:
 
 set_handlers(app)
 app.register_blueprint(jp_website)
+
+from canonicalwebteam.blog.app import BlogExtension
+blog_b = BlogExtension()
+blog_b.init_app(app, "Blog title", [2996], "snapcraft.io", "/blog")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
