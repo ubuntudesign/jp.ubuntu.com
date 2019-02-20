7eaa1fe2807fff4bd79fbde82606edeb30b401e0
Thomas Bille <thomas.bille@canonical.com>
2019-02-20 14:45:16 +0000

    ReAdd talisker to the codebase

--- webapp/app.py
+++ webapp/app.py
@@ -3,6 +3,7 @@ A Flask application for jp.ubuntu.com
 """
 
 import flask
+import talisker.flask
 from werkzeug.contrib.fixers import ProxyFix
 from werkzeug.debug import DebuggedApplication
 from werkzeug.routing import BaseConverter
@@ -22,6 +23,7 @@ app = flask.Flask(
     __name__, template_folder="../templates", static_folder="../static"
 )
 
+
 app.url_map.strict_slashes = False
 app.url_map.converters["regex"] = RegexConverter
 
@@ -29,11 +31,14 @@ app.wsgi_app = ProxyFix(app.wsgi_app)
 if app.debug:
     app.wsgi_app = DebuggedApplication(app.wsgi_app)
 
+talisker.flask.register(app)
+
 set_handlers(app)
 app.register_blueprint(jp_website)
 
 blog = BlogExtension()
 blog.init_app(app, "Ubuntu blog", [3184], "lang:jp", "/blog")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
