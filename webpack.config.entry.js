cef95b2adc631b23ca2cfb8a6fac4abf8b73b266
Peter Mahnke <peter.mahnke@canonical.com>
2020-07-27 15:43:05 +0100

    Add newsletter form to /blog

--- webpack.config.entry.js
+++ webpack.config.entry.js
@@ -1,4 +1,7 @@
 module.exports = {
   ["latest-news"]: "./static/js/latest-news.js",
   ["global-nav"]: "./static/js/global-nav.js",
+  ["serialize"]: "./static/js/third-party/serialize.js",
+  ["forms"]: "./static/js/forms.js",
+  ["form-validation"]: "./static/js/form-validation.js",
 };
