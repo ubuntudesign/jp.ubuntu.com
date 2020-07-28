e89f0e3c70e8e2fe8d867ab33fbeabd91264c377
Peter Mahnke <peter.mahnke@canonical.com>
2020-07-28 10:44:00 +0100

    Prettierd the js

--- static/js/form-validation.js
+++ static/js/form-validation.js
@@ -3,7 +3,7 @@
  * to disable the normal submit, and instead use backgroundSubmit
  */
 
-import serialize from './dist/serialize';
+import serialize from "./dist/serialize";
 
 const backgroundSubmitHandlerClosure = function () {
   return function (submitEvent) {
@@ -17,7 +17,7 @@ const backgroundSubmitHandlerClosure = function () {
 
     // Change the form's action location
     marketoForm.action =
-      'https://app-sjg.marketo.com/index.php/leadCapture/save2';
+      "https://app-sjg.marketo.com/index.php/leadCapture/save2";
 
     // Submit the form in the background
     backgroundSubmit(marketoForm);
@@ -26,17 +26,17 @@ const backgroundSubmitHandlerClosure = function () {
 
 const backgroundSubmit = function (marketoForm, submitCallback) {
   var request = new XMLHttpRequest();
-  var submitUrl = marketoForm.getAttribute('action');
+  var submitUrl = marketoForm.getAttribute("action");
   let formData = serialize(marketoForm);
 
-  request.open('POST', submitUrl, true);
+  request.open("POST", submitUrl, true);
 
   //Send the proper header information along with the request
-  request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
   // When request has finished, call the callback function
   if (submitCallback) {
-    request.addEventListener('readystatechange', function () {
+    request.addEventListener("readystatechange", function () {
       if (this.readyState == 4) {
         // Pass context and arguments on to submitCallback
         submitCallback.apply(this, arguments);
@@ -49,34 +49,34 @@ const backgroundSubmit = function (marketoForm, submitCallback) {
 
   // get the download asset if it exists
   var download_asset_url = marketoForm.querySelector(
-    'input[name=download_asset_url]'
+    "input[name=download_asset_url]"
   );
   if (download_asset_url != null) {
     download_asset_url = download_asset_url.value;
   }
 
   // get the return url if it exists to redirect users after
-  var return_url = marketoForm.querySelector('input[name=return_url]');
+  var return_url = marketoForm.querySelector("input[name=return_url]");
   if (return_url != null) {
     return_url = return_url.value;
   }
 
   // check if it is a dynamic modal form
-  var isModal = marketoForm.classList.contains('modal-form');
+  var isModal = marketoForm.classList.contains("modal-form");
 
   // check if it is a whitepaper
-  var isWhitepaper = marketoForm.classList.contains('whitepaper-form');
+  var isWhitepaper = marketoForm.classList.contains("whitepaper-form");
 
   // check if there is a thank you message to post
   var thankYouMessage = marketoForm.querySelector(
-    'input[name=thankyoumessage]'
+    "input[name=thankyoumessage]"
   );
   if (thankYouMessage != null) {
     thankYouMessage = thankYouMessage.value;
   }
 
   // reset form and captcha
-  if (!document.querySelector('.js-feedback-notification')) {
+  if (!document.querySelector(".js-feedback-notification")) {
     marketoForm.reset();
   }
 
@@ -108,10 +108,10 @@ const afterSubmit = function (
 ) {
   // Now start the download
   if (download_asset_url) {
-    var downloadFrame = document.createElement('iframe');
-    downloadFrame.setAttribute('src', download_asset_url);
-    downloadFrame.style.width = 0 + 'px';
-    downloadFrame.style.height = 0 + 'px';
+    var downloadFrame = document.createElement("iframe");
+    downloadFrame.setAttribute("src", download_asset_url);
+    downloadFrame.style.width = 0 + "px";
+    downloadFrame.style.height = 0 + "px";
     document.body.insertBefore(downloadFrame, document.body.childNodes[0]);
   }
 
@@ -125,11 +125,11 @@ const afterSubmit = function (
   // dynamic thank you HACK
   if (isModal) {
     document
-      .getElementsByClassName('js-pagination--3')[0]
-      .classList.add('u-hide');
+      .getElementsByClassName("js-pagination--3")[0]
+      .classList.add("u-hide");
     document
-      .getElementsByClassName('js-pagination--4')[0]
-      .classList.remove('u-hide');
+      .getElementsByClassName("js-pagination--4")[0]
+      .classList.remove("u-hide");
   }
 
   // add a thank-you notification to the page
@@ -137,28 +137,28 @@ const afterSubmit = function (
   if (return_url === null && isModal === false && isWhitepaper === false) {
     if (thankYouMessage === null) {
       thankYouMessage =
-        'Thank you<br />A member of our team will be in touch within one working day';
+        "Thank you<br />A member of our team will be in touch within one working day";
     }
-    var feedbackArea = document.querySelector('.js-feedback-notification');
+    var feedbackArea = document.querySelector(".js-feedback-notification");
     if (feedbackArea) {
       feedbackArea.innerHTML =
         '<div class="p-notification--positive"><p class="p-notification__response">' +
         thankYouMessage +
-        '</p></div>';
-      var inputs = marketoForm.querySelectorAll('input, button');
+        "</p></div>";
+      var inputs = marketoForm.querySelectorAll("input, button");
       for (var i = 0; i < inputs.length; i++) {
-        inputs[i].disabled = 'disabled';
+        inputs[i].disabled = "disabled";
       }
 
-      marketoForm.style.opacity = '.5';
+      marketoForm.style.opacity = ".5";
     } else {
       document
-        .getElementById('main-content')
+        .getElementById("main-content")
         .insertAdjacentHTML(
-          'afterbegin',
+          "afterbegin",
           '<div class="p-strip is-shallow u-no-padding--bottom"><div class="row"><div class="p-notification--positive"><p class="p-notification__response">' +
             thankYouMessage +
-            '</p></div></div></div>'
+            "</p></div></div></div>"
         );
       window.scrollTo(0, 0);
     }
@@ -170,41 +170,41 @@ const afterSubmit = function (
 };
 
 // attach handler to all forms
-let marketoForm = document.querySelectorAll('form[id^=mktoForm]');
+let marketoForm = document.querySelectorAll("form[id^=mktoForm]");
 marketoForm.forEach(function (form) {
-  form.addEventListener('submit', backgroundSubmitHandlerClosure());
+  form.addEventListener("submit", backgroundSubmitHandlerClosure());
 });
 
 // After submit has happened set a cookie and reveal the content
 function whitepaperAfterSubmit() {
-  setCookie(getCookieName(), 'true', 30);
+  setCookie(getCookieName(), "true", 30);
   revealContent();
-  window.location.hash = '#introduction';
+  window.location.hash = "#introduction";
 }
 
 function setCookie(cname, cvalue, exdays) {
   var d = new Date();
   d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  var expires = 'expires=' + d.toUTCString();
-  document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 // Returns a unique cookie name for this URL path
 function getCookieName() {
-  var encodedPath = window.location.pathname.split('/').join('-');
-  return 'formFilled' + encodedPath;
+  var encodedPath = window.location.pathname.split("/").join("-");
+  return "formFilled" + encodedPath;
 }
 
 function revealContent() {
-  var contentContainer = document.querySelector('.l-content');
+  var contentContainer = document.querySelector(".l-content");
   if (contentContainer) {
-    fetchContent('robotics/_content', contentContainer);
+    fetchContent("robotics/_content", contentContainer);
   }
 
   if (hiddenContent) {
     var content = document.querySelectorAll(
-      '.u-obfuscate p',
-      '.u-obfuscate li'
+      ".u-obfuscate p",
+      ".u-obfuscate li"
     );
     content.forEach(function (contentItem) {
       contentItem.innerHTML = reverseContent(contentItem.innerText);
@@ -213,15 +213,15 @@ function revealContent() {
   }
 
   // Remove the obfuscating styling
-  var obfuscateItems = document.querySelectorAll('.u-obfuscate');
+  var obfuscateItems = document.querySelectorAll(".u-obfuscate");
   obfuscateItems.forEach(function (obfuscateItem) {
-    obfuscateItem.classList.remove('u-obfuscate');
+    obfuscateItem.classList.remove("u-obfuscate");
   });
 
   // Hide the sign up form
-  var formElement = document.querySelector('.signup-form');
+  var formElement = document.querySelector(".signup-form");
   if (formElement) {
-    formElement.classList.add('u-hide');
+    formElement.classList.add("u-hide");
   }
 }
 
@@ -232,28 +232,28 @@ function fetchContent(url, container) {
     })
     .then(function (text) {
       container.innerHTML = text;
-      container.classList.add('u-reveal');
+      container.classList.add("u-reveal");
     })
     .catch(function (error) {
-      console.log('Request failed', error);
+      console.log("Request failed", error);
     });
 }
 
 let hiddenContent = true;
 
 function reverseContent(contentString) {
-  var splitContent = contentString.split('');
+  var splitContent = contentString.split("");
   var reverseArray = splitContent.reverse();
-  var reversedString = reverseArray.join('');
+  var reversedString = reverseArray.join("");
   return reversedString;
 }
 
 function getCookie(cname) {
-  var name = cname + '=';
-  var ca = document.cookie.split(';');
+  var name = cname + "=";
+  var ca = document.cookie.split(";");
   for (var i = 0; i < ca.length; i++) {
     var c = ca[i];
-    while (c.charAt(0) == ' ') {
+    while (c.charAt(0) == " ") {
       c = c.substring(1);
     }
     if (c.indexOf(name) == 0) {
@@ -266,7 +266,7 @@ function getCookie(cname) {
 function hideContent() {
   hiddenContent = true;
   // Baffle to obfucast the text
-  var content = document.querySelectorAll('.u-obfuscate p', '.u-obfuscate li');
+  var content = document.querySelectorAll(".u-obfuscate p", ".u-obfuscate li");
   content.forEach(function (contentItem) {
     contentItem.innerText = reverseContent(contentItem.innerHTML);
   });
