5010500a266d186e35c6877c466eb94babf248a5
minkyngkm <min.kim@canonical.com>
2022-01-13 12:02:14 +0000

    Remove form-validation.js

--- webpack.config.entry.js
+++ webpack.config.entry.js
@@ -2,5 +2,4 @@ module.exports = {
   ["cookie-policy"]: "./static/js/cookie-policy.js",
   ["latest-news"]: "./static/js/latest-news.js",
   ["global-nav"]: "./static/js/global-nav.js",
-  ["forms"]: ["./static/js/forms.js", "./static/js/form-validation.js"],
 };
